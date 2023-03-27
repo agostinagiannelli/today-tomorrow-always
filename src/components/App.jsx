bca46c80696801fe9631b28515c8ac060db4fd33
Agostina Giannelli <agostinagiannelli@gmail.com>
2023-03-27 17:15:39 +0200

    PreEntrega1Giannelli

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './NavBar/NavBar.jsx'
 import ItemListContainer from './ItemListContainer/ItemListContainer.jsx'
+
 const App = () => {
   return (
     <div>
